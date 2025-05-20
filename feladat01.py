0142be27640402df8ed43bcc4017b80d604220ef
markbojtos <markbojtos08@gmail.com>
2025-05-20 13:24:38 +0200

    01kesz

--- feladat01.py
+++ feladat01.py
@@ -1,6 +1,9 @@
-print("Hello")
-print("csao")
-meglepi=input("Mire: ")
-kutyakszama=int(input("Hány kutyus: "))
-osszeg=700*kutyakszama
-print(osszeg)
+import datetime
+miregyujt=input("Mire gyűjti Anna a pénzt? ")
+hanykutya=int(input("Hány kutyát sétáltatott Anna? "))
+print(f"Anna {hanykutya*20//60}:{hanykutya*20%60}-t töltött kutyasétáltatással.")
+osszpenz=hanykutya*700
+if osszpenz>=5000:
+    print(f"Anna {hanykutya} kutyát sétáltatott {hanykutya*20//60}:{hanykutya*20%60} alatt, ezért {osszpenz} Ft-ot kapott. Ez elég a {miregyujt} megvásárlásához.")
+else:
+    print(f"Anna {hanykutya} kutyát sétáltatott {hanykutya*20//60}:{hanykutya*20%60} alatt, ezért {osszpenz} Ft-ot kapott. Ez még nem elég a {miregyujt} megvásárlásához.")
